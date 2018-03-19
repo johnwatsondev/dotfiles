12469f53f6ecf3be8c62e0fc37d25ccda987558b
JohnWatsonDev <johnwatsondev.com@gmail.com>
2018-03-19 22:10:53 +0800

    Update and rename install.sh to install_ohmyzsh.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# on my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# software with brew cask
-brew cask install iterm2 google-chrome sourcetree keepingyouawake macdown iina font-hack
---
+++ install_ohmyzsh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# on my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completion
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
