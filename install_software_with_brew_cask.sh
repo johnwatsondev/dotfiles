18b4a091da3ea72de24b4783a2b22cb4ed6f2c23
JohnWatsonDev <johnwatsondev.com@gmail.com>
2018-04-10 23:52:20 +0800

    Update install_software_with_brew_cask.sh

--- install_software_with_brew_cask.sh
+++ install_software_with_brew_cask.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 brew cask install iterm2 sourcetree keepingyouawake macdown iina
+brew cask install qlmarkdown webpquicklook quicklook-json qlimagesize qlstephen qlcolorcode
 
 brew tap caskroom/fonts
 brew cask install font-hack
