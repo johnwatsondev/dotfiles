58b88b63cd302df44f267180a60cf38769bbc70e
JohnWatsonDev <johnwatsondev.com@gmail.com>
2019-01-28 10:47:58 +0800

    Update install_software_with_brew.sh

--- install_software_with_brew.sh
+++ install_software_with_brew.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-brew install cmake git git-flow-avh ccat dex2jar apktool vim tig ripgrep aria2 graphviz jadx pidcat tree
+brew install cmake git git-flow-avh ccat dex2jar apktool vim tig ripgrep aria2 graphviz jadx tree
