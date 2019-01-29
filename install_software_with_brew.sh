3d159207542587b01276045f4789743cf9f434f3
JohnWatsonDev <johnwatsondev.com@gmail.com>
2019-01-30 00:19:37 +0800

    Update install_software_with_brew.sh

--- install_software_with_brew.sh
+++ install_software_with_brew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew install cmake git git-flow-avh ccat vim tig ripgrep aria2 graphviz tree
+brew install cmake git git-flow-avh ccat jenv vim tig ripgrep aria2 graphviz tree
 
 brew install --HEAD pidcat
 
